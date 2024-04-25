2e585ab2d24ddef92ef914bdb7e8f795ce071784
Michael Heimes <michael@cairndigitalmedia.com>
2024-04-25 18:09:13 -0400

    Init, dev

---
+++ inc/acf-options.php
@@ -0,0 +1,11 @@
+<?php
+if( function_exists('acf_set_options_page_title') ) {		
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+}
